899fadea110154163bd232c5f66a90fba4338e95
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-02-07 23:26:53 +0600

    problem 2 solved

---
+++ problem-2.js
@@ -0,0 +1,22 @@
+ 
+
+
+// create a function is named checkName(), it takes input as string ,it can be capital or small letter .
+function checkName(name){
+// return invalid its type either than string.
+  if(typeof name !== "string"){
+    return "invalid";
+  }
+
+  const lastLetter = name[name.length -1].toLowerCase();
+// its checks each given input's last letter , if this letter is "a, y, i, e, o, u, w" one of this, the function will return "Good Name" otherwise it will return "Bad Name"
+
+  if("ayieouw".includes(lastLetter)){
+    return "Good Name"
+  }
+  else{
+    return "Bad name"
+  }
+}
+const nameStr = ["Rashed"];
+console.log(checkName(nameStr));
